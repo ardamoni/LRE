7d616c543e6c31055ef248d4cdd44b7f2cd60e7c
ardamoni <ardamoni@gmail.com>
2013-08-26 08:29:04 +0000

    Update functionalities, db and data
    Carry data from one old table to the other one.  These populate files,
    should be used with care, since it can duplicate in table where it is
    copied to.

---
+++ php/Helpers/populate_business_payments.php
@@ -0,0 +1,48 @@
+<?php
+
+	/*	
+	 * 	this file is used to insert the revenue collection into tables
+	 */
+
+	// DB connection
+	require_once( "../lib/configuration.php" );
+	
+	echo "start: ", "<br>";
+	
+	$q1 = mysql_query("SELECT * from `payments_business` WHERE `id` > 1 ");
+		
+	$i=1;
+	while($BOR = mysql_fetch_array($q1))
+	{
+		mysql_query("INSERT INTO `business_payments` (	`id`,
+														`upn`, 
+														`subupn`,
+														`payment_date`,
+														`payment_value`,
+														`station_payment`,
+														`receipt_payment`,
+														`type_payment`,
+														`payer`,
+														`paid_for`
+														) 
+											VALUES 	( 	NULL,
+														'".$BOR['upn']."',
+														'".$BOR['subupn']."',
+														'".$BOR['date_payment']."',
+														'".$BOR['payment']."',
+														'".$BOR['station_payment']."',
+														'".$BOR['receipt_payment']."',
+														'".$BOR['type_payment']."',
+														'".$BOR['payer']."',
+														'".$BOR['paid_for']."'
+													)");
+		echo $i, ": ", $BOR['upn'], " & ", $BOR['subupn'], "<br>";
+		$i++;
+	}
+	
+	// update empty columns
+	mysql_query( "UPDATE `business_payments` SET `districtid` = '1840'" );
+	
+	
+	
+?>
